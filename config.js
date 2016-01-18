109448a9dbc6e0d855c311a300e2a15b0039127c
fanweiya <fanweiya@users.noreply.github.com>
2016-01-18 17:13:22 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 config = {
-    blog_name: "涵曦 ● 博客",
-    github_username: "hanxi", // github 账号名
+    blog_name: "my issues blog",
+    github_username: "fanweiya", // github 账号名
     github_repo: "blog",      // github 建立的 repo 名
-    per_page: 10,             // 首页每次载入文章列表数量
+    per_page: 15,             // 首页每次载入文章列表数量
 
     // 请求量大时需要在 github 后台单独设置一个读取公开库的 token
     // 将配置好的 access token 拆分成两个字符串相加
-    access_token: "dbfe3dd66ee904988f2b"+"aaaf9bf49a03b6e3fd62"
-};
+    access_token: "27d901e99842802bb5cc"+"71b81e2872aa7b32030d"
+};                 
