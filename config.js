1d531d91016c3d3f2f39b067617f26d7560321d9
fanweiya <fanweiya@users.noreply.github.com>
2016-02-02 09:02:35 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 config = {
-    blog_name: "my issues blog",
+    blog_name: "我的博客",
     github_username: "fanweiya", // github 账号名
     github_repo: "blog",      // github 建立的 repo 名
     per_page: 15,             // 首页每次载入文章列表数量
